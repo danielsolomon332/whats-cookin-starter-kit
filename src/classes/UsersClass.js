9c30434436e1a0fcda00f2ba4db5ee3e8bda095d
Grace Gardner <graceirisgardner@gmail.com>
2021-12-12 14:43:16 -0700

    Add unfavorite functionality and home button fix

--- src/classes/UsersClass.js
+++ src/classes/UsersClass.js
@@ -5,18 +5,36 @@ class User {
     this.pantry = user.pantry;
     this.favoriteRecipes = [];
     this.toCook = [];
+    this.tagsList = [];
   }
 
   addFavorite(recipe) {
     this.favoriteRecipes.push(recipe);
+    recipe.tags.forEach(tag => {
+      if(!this.tagsList.includes(tag)){
+        this.tagsList.push(tag);
+      }
+    })
   }
 
   removeFavorite(recipe) {
     if (this.favoriteRecipes.includes(recipe)) {
       const deleteIndex = this.favoriteRecipes.indexOf(recipe);
       this.favoriteRecipes.splice(deleteIndex, 1)
+      this.tagsList = this.tagsList.filter(tag => {
+       return !recipe.tags.includes(tag)
+      })
     }
   }
+
+  filterByTags(tags) {
+    const result = this.favoriteRecipes.filter(recipe => {
+    if (tags.every(tag => {return recipe.tags.includes(tag)})) {
+      return recipe
+    }
+  });
+    return result
+  };
 
   addToCook(recipe) {
     this.toCook.push(recipe);
